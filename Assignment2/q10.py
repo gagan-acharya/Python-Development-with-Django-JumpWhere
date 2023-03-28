21425bae32a46c58fb8a3cba3b25d676bf6eb966
gagan-acharya <95954879+gagan-acharya@users.noreply.github.com>
2023-03-28 11:31:45 +0530

    Update q10.py

--- Assignment2/q10.py
+++ Assignment2/q10.py
@@ -1,13 +1,9 @@
 def printParenthesis(str, n):
 	if(n > 0):
-		_printParenthesis(str, 0,
-						n, 0, 0)
+		_printParenthesis(str, 0,n, 0, 0)
 	return
 
-
-def _printParenthesis(str, pos, n,
-					open, close):
-
+def _printParenthesis(str, pos, n,open, close):
 	if(close == n):
 		for i in str:
 			print(i, end="")
@@ -16,12 +12,10 @@ def _printParenthesis(str, pos, n,
 	else:
 		if(open > close):
 			str[pos] = '}'
-			_printParenthesis(str, pos + 1, n,
-							open, close + 1)
+			_printParenthesis(str, pos + 1, n,open, close + 1)
 		if(open < n):
 			str[pos] = '{'
-			_printParenthesis(str, pos + 1, n,
-							open + 1, close)
+			_printParenthesis(str, pos + 1, n,open + 1, close)
 n = 3
 str = [""] * 2 * n
 printParenthesis(str, n)
